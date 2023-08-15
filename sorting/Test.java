430891cd951a75c6c8469bd840b322e97242fd01
Thadeus Bates <batesthadeus@gmail.com>
2023-08-14 23:44:31 -0700

    More flexible tests

--- sorting/Test.java
+++ sorting/Test.java
@@ -17,15 +17,20 @@ public class Test
     AlegSort alegSort = new AlegSort();
     BubbleSort bubbleSort = new BubbleSort();
     
-    static int testSize = 100000000;
+    static int alg1Total = 0;
+    static int alg2Total = 0;
+    static int testCount = 0;
+    static int testSize = 200000000;
     public static void main(String[] args) 
     {
         Test test = new Test();
         System.out.println("\n\nTesting..."); 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 10; i++)
         {
             test.randTest(testSize);
         }
+        System.out.printf("Tested algorithm 1 took %s milliseconds on average\n", alg1Total / testCount);
+        System.out.printf("Tested algorithm 2 took %s milliseconds on average\n", alg2Total / testCount);
     }
 
     public Test() 
@@ -34,6 +39,7 @@ public class Test
     }
     public void randTest(int testSize) 
     {
+        this.testCount++;
         int[] testAnswer = new int[testSize];
         int[] testCase1 = new int[testSize];
         int[] testCase2 = new int[testSize];
@@ -56,19 +62,21 @@ public class Test
         {
             testCase2[i] = testCase1[i];
         }
-        
+        System.out.printf("Testcase %s generated successfully\n", this.testCount);
         long startTime;
         long timeElapsed;
         
         startTime = System.currentTimeMillis();
         quickSort.sort(testCase1);
         timeElapsed = System.currentTimeMillis() - startTime;
+        this.alg1Total += timeElapsed; 
         System.out.printf("Tested algorithm 1 successfully with array size %s in %s milliseconds\n", testSize, timeElapsed);
         checkForError(testCase1, testAnswer, 1);
         
         startTime = System.currentTimeMillis();
         mergeSort.sort(testCase2);
         timeElapsed = System.currentTimeMillis() - startTime;
+        this.alg2Total += timeElapsed; 
         System.out.printf("Tested algorithm 2 successfully with array size %s in %s milliseconds\n", testSize, timeElapsed);
         checkForError(testCase2, testAnswer, 2);
         System.out.println();
